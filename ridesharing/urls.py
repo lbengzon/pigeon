82229a1c1166ae52551ab558fee8283c515540da
Cristian Saguil <crsaguil@rogomi.com>
2016-12-26 20:23:20 +0800

    added individual ride view

--- ridesharing/urls.py
+++ ridesharing/urls.py
@@ -6,6 +6,7 @@ from ridesharing.models import Ride
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Ride.objects.all().order_by("-date")
                                 , template_name = 'ridesharing/rideList.html')),
-    #/ridesharing/ride/add
     url(r'ride/add$', views.ride_create, name='ride-add'),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Ride,
+                                            template_name = 'ridesharing/ride.html'))
 ]
