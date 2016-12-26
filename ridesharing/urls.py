07bfe105acbe6dc6ed2dd829aa2d90fa02e50ec3
Cristian Saguil <crsaguil@rogomi.com>
2016-12-26 17:42:23 +0800

    added logo, reformtted ride posts

--- ridesharing/urls.py
+++ ridesharing/urls.py
@@ -4,7 +4,8 @@ from django.views.generic import ListView, DetailView
 from ridesharing.models import Ride
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Ride.objects.all().order_by("-date"), template_name = 'ridesharing/rideList.html')),
+    url(r'^$', ListView.as_view(queryset=Ride.objects.all().order_by("-date")
+                                , template_name = 'ridesharing/rideList.html')),
     #/ridesharing/ride/add
     url(r'ride/add$', views.ride_create, name='ride-add'),
 ]
