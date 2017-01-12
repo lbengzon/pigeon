47e7013fcd4af7541dc880a706cee72fd20495a1
Cristian Saguil <crsaguil@rogomi.com>
2017-01-12 21:17:23 +0800

    added home/about page

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render_to_response
 # Create your views here.
 def show_home(request):
     return render(request, 'home/home.html')
+
+def show_about(request):
+    return render(request, 'home/about.html')
