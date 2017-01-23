50dbec9fba20adfccf4cfa121204fc12f43a257b
Leandro <leandro@Leandros-MacBook-Pro-3.local>
2017-01-23 11:39:08 -0500

    added origin to ride

--- ridesharing/models.py
+++ ridesharing/models.py
@@ -14,6 +14,7 @@ class Ride(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=13, default="")  # validators should be a list
+    origin = models.CharField(max_length=140, default="Colgate")
     destination = models.CharField(max_length = 140)
     seats_available = models.PositiveIntegerField(default=0)
     approved = models.BooleanField(default=False)
